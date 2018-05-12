09c0cc463850d6fa0514fc3b8e779c86195e5eab
Fabian Miliani <fabmil22@gmail.com>
2018-05-12 12:26:44 +0200

    con form reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -18,7 +18,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user.ts
@@ -0,0 +1,5 @@
+export class User {
+
+    nombre: string;
+    dni: string;
+}
