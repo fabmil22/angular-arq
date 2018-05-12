73a817b88a115a0561188460d7641c0e7dd7b3ee
Fabian Miliani <fabmil22@gmail.com>
2018-05-12 11:50:03 +0200

    secund

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { Form1Component } from './components/form1/form1.component';
+import { Form2Component } from './components/form2/form2.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Form1Component,
+    Form2Component,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
